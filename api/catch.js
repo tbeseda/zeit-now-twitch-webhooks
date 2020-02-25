b5235bb1c2c91a455c1bb141a4ce666ac7bed7cf
Taylor Beseda <tbeseda@gmail.com>
2020-02-25 16:58:26 -0700

    add Segment and track events

--- api/catch.js
+++ api/catch.js
@@ -1,15 +1,36 @@
+const Analytics = require('analytics-node');
+const analytics = new Analytics(process.env.SEGMENT_WRITE_KEY, { flushAt: 1 });
+
 module.exports = (request, response) => {
-  console.log('Catching!');
+  console.log('Catching Wbhook');
 
   if (request.query && request.query['hub.challenge']) {
     console.log(`Responding to challenge ${request.query['hub.challenge']}`);
+
+    analytics.track({
+      userId: 1337,
+      event: 'Webhook Caught With Challenge',
+      properties: request.query,
+    });
+
     response.send(request.query['hub.challenge']);
   } else {
-    console.log({
-      body: request.body,
-      query: request.query,
-      cookies: request.cookies,
-    });
+    if (request.body) {
+      console.log('Webhook payload', request.body);
+
+      analytics.track({
+        userId: 1337,
+        event: 'Webhook Caught',
+        properties: request.body.data,
+      });
+    } else {
+      console.log('Webhook without payload.');
+
+      analytics.track({
+        userId: 1337,
+        event: 'Webhook Caught Without Payload',
+      });
+    }
 
     response.send('teacup');
   }
